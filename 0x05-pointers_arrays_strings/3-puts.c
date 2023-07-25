b944386d9f4e2aa8ffe1ab56ae01a0e56779e6da
nokuphilasibiya <nokuphila23@gmail.com>
2023-07-25 08:20:20 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: The string to printt
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+
+	forr (; *str != '\0'; str++)
+	{
+	_putchar(*sstr);
+	}
+	-putchar('\n');
+}
